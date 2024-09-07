95f07929f925f5efc7465a0be45d960aa5ae2c36
masterPeaceJs <bert3331@mail.ru>
2024-09-07 22:47:52 +0300

    smallest integer in array commit

---
+++ 8 kyu/smallest integer in array.js
@@ -0,0 +1,22 @@
+// Given an array of integers your solution should find the smallest integer.
+
+// For example:
+
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+
+arr = [3, 56, 2, 154, 343, 1, 3];
+
+function findSmallestInt(arr) {
+    let min = arr[0];
+      for (let i = 0; i < arr.length; i++) {
+        if (min > arr[i]) {
+          min = arr[i];
+        } 
+      }
+    return min;
+}
+
+console.log(findSmallestInt(arr));
